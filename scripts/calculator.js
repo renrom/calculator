f77250d054909c4ee869bec3f333fb37fc1b8c59
¨rene@instinkt.nl¨ <rene@instinkt.nl>
2024-02-04 12:34:13 +0100

    Almost dont all the logic.

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,7 +1,8 @@
 
 let number1 = "";
 let number2 = "";
-
+let previousOperator = "";
+let usedOperator = "";
 let isOperatorClicked = false;
 
 
@@ -18,86 +19,164 @@ btn0 = document.querySelector(".btn-0");
 
 btnPercent = document.querySelector(".btn-percent");
 btnDivide = document.querySelector(".btn-divide");
-btnplusmin = document.querySelector(".btn-plusmin");
-btnMulitply = document.querySelector(".btn-multiply");
+btnPlusMin = document.querySelector(".btn-plusmin");
+btnMultiply = document.querySelector(".btn-multiply");
 btnMinus = document.querySelector(".btn-minus");
 btnPlus = document.querySelector(".btn-plus");
-btnRresult = document.querySelector(".btn-result");
+btnResult = document.querySelector(".btn-result");
 btnC = document.querySelector(".btn-c");
-btnCe = document.querySelector(".btn-ce");
+btnAc = document.querySelector(".btn-ac");
+btnDot = document.querySelector(".btn-dot");
 
-btn1.addEventListener("click", function () {
-    if (isOperatorClicked) {
-        number2 += "1";   
-        displayResult(number2);
-    } else {
-        number1 += "1";
-        displayResult(number1);
-    }
+btn1.addEventListener("click", () => handleNumberClick("1"));
+btn2.addEventListener("click", () => handleNumberClick("2"));
+btn3.addEventListener("click", () => handleNumberClick("3"));
+btn4.addEventListener("click", () => handleNumberClick("4"));
+btn5.addEventListener("click", () => handleNumberClick("5"));
+btn6.addEventListener("click", () => handleNumberClick("6"));
+btn7.addEventListener("click", () => handleNumberClick("7"));
+btn8.addEventListener("click", () => handleNumberClick("8"));
+btn9.addEventListener("click", () => handleNumberClick("9"));
+btn0.addEventListener("click", () => handleNumberClick("0"));
+btnDot.addEventListener("click", () => handleNumberClick("."));
 
-});
-
-btn2.addEventListener("click", function () {
-    displayResult(2);
-    
-});
 
 btnC.addEventListener("click", function () {
     resetNumbers();
     displayResult(0);
-    
+
 });
 
-btnDivide.addEventListener("click", function () {
+btnAc.addEventListener("click", function () {
+    resetNumbers();
+    displayResult(0);
+
+});
+
+btnDivide.addEventListener("click", () => handleOperandClick("/"));
+btnMinus.addEventListener("click", () => handleOperandClick("-"));
+btnPlus.addEventListener("click", () => handleOperandClick("+"));
+btnMultiply.addEventListener("click", () => handleOperandClick("*"));
+btnResult.addEventListener("click", () => handleOperandClick("="));
+
+
+btnPlusMin.addEventListener("click", function () {
     isOperatorClicked = true;
-    
+
 });
 
 function resetNumbers() {
-    let number1 = "";
-    let number2 = "";
+    number1 = "";
+    number2 = "";
+    previousOperator = "";
+    usedOperator = "";
+    isOperatorClicked = false;
 }
 
+function operate(operator, inputNumberOne, inputNumberTwo) {
+    let numberOne = 0;
+    let numberTwo = 0;
+    numberOne = parseFloat(inputNumberOne);
+    numberTwo = parseFloat(inputNumberTwo);
 
-function operate(operator,inputNumberOne, inputNumberTwo) {
-
-    // add
-    if (operator === "+") {
-        calcResult = inputNumberOne + inputNumberTwo;
+    if (operator == "+") {
+        calcResult = numberOne + numberTwo;
     };
 
     if (operator === "-") {
-        calcResult =  inputNumberOne - inputNumberTwo;
+        calcResult = numberOne - numberTwo;
 
     };
 
     if (operator === "*") {
-        calcResult =  inputNumberOne * inputNumberTwo;
+        calcResult = numberOne * numberTwo;
 
     };
 
     if (operator === "/") {
-        calcResult =  inputNumberOne / inputNumberTwo;
+
+        if (numberTwo === 0) {
+            resetNumbers();
+            return "!!ERROR!!";
+        } else {
+            calcResult = numberOne / numberTwo;
+        }
+
 
     };
 
+
+    calcResult = parseFloat(calcResult).toFixed(9).replace(/0+$/, '').replace(/\.+$/, '');
     return calcResult;
+}
 
+function displayResult(number) {
+    const displayresult = document.querySelector('.calculator-display');
+
+    displayresult.innerHTML = number;
 
 }
 
-function displayResult(number){
-        const displayresult = document.querySelector('.calculator-display');
+function handleNumberClick(number) {
 
-        displayresult.innerHTML = number;
- 
+    if (!isOperatorClicked) {
+        if (number2 !== "") {
+            if (number2.length < 10) {
+                number2 += number;
+                displayResult(number2);
+                return;
+            } else {
+                return;
+            };
+        }
+        if (number1.length < 10) {
+            number1 += number;
+            displayResult(number1);
+        } else {
+            return;
+        };
+    } else {
+        isOperatorClicked = false;
+
+        if (number2 !== "") {
+
+            /*                    calculatedResult = operate(usedoperator, number1, number2);
+                                displayResult(calculatedResult);
+                                number2 = "";
+                                number1 = calculatedResult;
+                                usedoperator = ""; */
+
+        } else {
+            if (number2.length < 10) {
+                number2 += number;
+                displayResult(number2);
+                return;
+            };
+        }
+    };
 }
 
+function handleOperandClick(operator) {
+    isOperatorClicked = true;
 
+    if (number2 === "") {
+        previousOperator = operator;
+        return;
+    }
 
+    if (number1 !== "" && number2 !== "") {
+        calculatedResult = operate(previousOperator, number1, number2);
+        displayResult(calculatedResult);
+        number2 = "";
+        number1 = calculatedResult;
+        previousOperator = operator;
 
-displayResult(operate("+",12323,342345));
+        if (operator === "=") {
+            resetNumbers();
+        }
 
-console.log(operate("+",5,7));
+    }
+
+}
 
 
