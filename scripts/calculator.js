96a51a21dd01e07c439ee79435a421ae38af850c
¨rene@instinkt.nl¨ <rene@instinkt.nl>
2024-02-04 15:01:58 +0100

    Added also keyboad support.

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -4,6 +4,7 @@ let number2 = "";
 let previousOperator = "";
 let usedOperator = "";
 let isOperatorClicked = false;
+let clearResult = false;
 
 
 btn1 = document.querySelector(".btn-1");
@@ -38,32 +39,119 @@ btn7.addEventListener("click", () => handleNumberClick("7"));
 btn8.addEventListener("click", () => handleNumberClick("8"));
 btn9.addEventListener("click", () => handleNumberClick("9"));
 btn0.addEventListener("click", () => handleNumberClick("0"));
-btnDot.addEventListener("click", () => handleNumberClick("."));
 
+document.addEventListener("keydown", (keypressed) => {
+
+    if (keypressed.code === 'Digit1' || keypressed.code === "Numpad1") {
+        handleNumberClick("1");
+    }
+
+    if (keypressed.code === 'Digit2' || keypressed.code === "Numpad2") {
+        handleNumberClick("2");
+    }
+    if (keypressed.code === 'Digit3' || keypressed.code === "Numpad3") {
+        handleNumberClick("3");
+
+    }
+    if (keypressed.code === 'Digit4' || keypressed.code === "Numpad4") {
+        handleNumberClick("4");
+    }
+
+    if (keypressed.code === 'Digit5' || keypressed.code === "Numpad5") {
+        handleNumberClick("5");
+    }
+
+    if (keypressed.code === 'Digit6' || keypressed.code === "Numpad6") {
+        handleNumberClick("6");
+    }
+    if (keypressed.code === 'Digit7' || keypressed.code === "Numpad7") {
+        handleNumberClick("7");
+    }
+    if (keypressed.code === 'Digit8' || keypressed.code === "Numpad8") {
+        handleNumberClick("8");
+    }
+    if (keypressed.code === 'Digit9' || keypressed.code === "Numpad9") {
+        handleNumberClick("9");
+    }
+    if (keypressed.code === 'Digit0' || keypressed.code === "Numpad0") {
+        handleNumberClick("0");
+    }
+
+    if (keypressed.code === "NumpadAdd") {
+        handleOperandClick("+");
+    }
+
+
+    if (keypressed.code === 'Minus' || keypressed.code === "NumpadSubtract") {
+        handleOperandClick("-");
+    }
+
+    if (keypressed.code === "NumpadMultiply") {
+        handleOperandClick("*");
+    }
+
+    if (keypressed.code === 'Slash' || keypressed.code === "NumpadDivide") {
+        handleOperandClick("/");
+    }
+
+    if (keypressed.code === 'Period' || keypressed.code === "NumpadDecimal") {
+        handleOperandClick(".");
+    }
+
+    if (keypressed.code === 'Equal' || keypressed.code === "NumpadEnter") {
+        handleResult();
+    }
+
+    if (keypressed.code === 'Delete'){
+        resetLastNumber();
+    }
+
+});
+
+
+function handleDotClicked() {
+    if (number2 !== "") {
+        if (number2.indexOf(".") === -1) {
+            handleNumberClick(".");
+        }
+    } else {
+        if (number1.indexOf(".") === -1) {
+            handleNumberClick(".");
+        }
+
+    }
+}
+
+
+btnDot.addEventListener("click", function () {
+    handleDotClicked();
+});
 
 btnC.addEventListener("click", function () {
-    resetNumbers();
-    displayResult(0);
-
+    resetLastNumber();
 });
 
 btnAc.addEventListener("click", function () {
     resetNumbers();
     displayResult(0);
-
 });
 
 btnDivide.addEventListener("click", () => handleOperandClick("/"));
 btnMinus.addEventListener("click", () => handleOperandClick("-"));
 btnPlus.addEventListener("click", () => handleOperandClick("+"));
 btnMultiply.addEventListener("click", () => handleOperandClick("*"));
-btnResult.addEventListener("click", () => handleOperandClick("="));
-
+btnResult.addEventListener("click", () => handleResult());
 
 btnPlusMin.addEventListener("click", function () {
-    isOperatorClicked = true;
-
+    displayResult("Not Yet");
+    resetNumbers();
 });
+
+btnPercent.addEventListener("click", function () {
+    displayResult("Not Yet");
+    resetNumbers();
+});
+
 
 function resetNumbers() {
     number1 = "";
@@ -71,6 +159,17 @@ function resetNumbers() {
     previousOperator = "";
     usedOperator = "";
     isOperatorClicked = false;
+}
+
+function resetLastNumber() {
+    if (number2 !== "") {
+        number2 = "";
+        isOperatorClicked = true;
+
+    } else {
+        number1 = "";
+    }
+    displayResult(0);
 }
 
 function operate(operator, inputNumberOne, inputNumberTwo) {
@@ -97,7 +196,7 @@ function operate(operator, inputNumberOne, inputNumberTwo) {
 
         if (numberTwo === 0) {
             resetNumbers();
-            return "!!ERROR!!";
+            return "error";
         } else {
             calcResult = numberOne / numberTwo;
         }
@@ -112,7 +211,6 @@ function operate(operator, inputNumberOne, inputNumberTwo) {
 
 function displayResult(number) {
     const displayresult = document.querySelector('.calculator-display');
-
     displayresult.innerHTML = number;
 
 }
@@ -138,15 +236,7 @@ function handleNumberClick(number) {
     } else {
         isOperatorClicked = false;
 
-        if (number2 !== "") {
-
-            /*                    calculatedResult = operate(usedoperator, number1, number2);
-                                displayResult(calculatedResult);
-                                number2 = "";
-                                number1 = calculatedResult;
-                                usedoperator = ""; */
-
-        } else {
+        if (number2 === ""){
             if (number2.length < 10) {
                 number2 += number;
                 displayResult(number2);
@@ -155,6 +245,20 @@ function handleNumberClick(number) {
         }
     };
 }
+
+
+function handleResult() {
+    if (number1 !== "" && number2 !== "") {
+        calculatedResult = operate(previousOperator, number1, number2);
+        displayResult(calculatedResult);
+        resetNumbers();
+        number1 = calculatedResult;
+        
+        
+    }
+};
+
+
 
 function handleOperandClick(operator) {
     isOperatorClicked = true;
@@ -170,13 +274,7 @@ function handleOperandClick(operator) {
         number2 = "";
         number1 = calculatedResult;
         previousOperator = operator;
-
-        if (operator === "=") {
-            resetNumbers();
-        }
-
     }
-
 }
 
 
